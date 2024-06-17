d36344af33d0aeb3f2602be999d99e5f5320ece6
Tafari Excell <excell.tafari@gmail.com>
2024-06-16 23:29:42 -0400

    commented out lines 6-8

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = require("./app.js");
 require("dotenv").config();
 const PORT = process.env.PORT;
 
+// app.get("api", (req, res) => {
+//   res.json({message:"Hello World"});
+// })
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
